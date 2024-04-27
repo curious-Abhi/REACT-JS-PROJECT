3f0c5707962a47a822a32043d8230de44f225d89
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-04-27 13:01:27 +0530

    files updated

---
+++ news -app/src/components/NewsBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsBoard = () => {
+  return (
+    <div>
+        <h2 className='text-center'>Latest <span className=''>News</span></h2>
+    </div>
+  )
+}
+
+export default NewsBoard

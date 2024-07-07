292d29fe6f83838c39a9066034d68f1745a90abf
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-07-07 19:54:59 +0530

    files updated

--- 03 Calculator/src/App.jsx
+++ 03 Calculator/src/App.jsx
@@ -1,35 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+const App=()=>{
+  const [input,setInput]=useState('');
+  const [result,setResult]=useState('');
+
+  const handleClick=(value)=>{
+    setInput(input+value)
+  };
+
+  const handleClear=()=>{
+    setInput('');
+    setResult('');
+  };
+
+  const handleCalculate=()=>{
+    try{
+      setResult(eval(input));
+    }catch(error){
+      setResult('Error')
+    }
+  }
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <div className='calculator'>
+          <input type='text' value={input} readOnly></input>
+          <input type='text' value={result} readOnly></input>
+          <div className='buttons'>
+  
+            <button onClick={()=>{handleClick('1')}}>1</button>
+            <button onClick={()=>{handleClick('2')}}>2</button>
+            <button onClick={()=>{handleClick('3')}}>3</button>
+            <button onClick={()=>{handleClick('+')}}>+</button>
+            <button onClick={()=>{handleClick('4')}}>4</button>
+            <button onClick={()=>{handleClick('5')}}>5</button>
+            <button onClick={()=>{handleClick('6')}}>6</button>
+            <button onClick={()=>{handleClick('-')}}>-</button>
+            <button onClick={()=>{handleClick('7')}}>7</button>
+            <button onClick={()=>{handleClick('8')}}>8</button>
+            <button onClick={()=>{handleClick('9')}}>9</button>
+            <button onClick={()=>{handleClick('*')}}>*</button>
+            <button onClick={()=>{handleClick('0')}}>0</button>
+            <button onClick={()=>{handleClick('.')}}>.</button>
+
+            <button onClick={handleCalculate}>=</button>
+            <button onClick={()=>{handleClick('/')}}>/</button>
+            <button onClick={handleClear}>C</button>
+          </div>
+  
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
-}
+};
 
 export default App
